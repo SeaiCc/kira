77493490d723bb0e2b33d829b87d6363ae876b44
cuitao <353615653@qq.com>
2026-07-02 11:18:55 +0800

    [Feat] Add frontend.

---
+++ frontend/app/api/index.ts
@@ -0,0 +1,6 @@
+export * from "./posts";
+export * from "./albums";
+export * from "./categories";
+export * from "./projects";
+export * from "./comments";
+export * from "./site-config";
---
+++ frontend/next.config.ts
@@ -0,0 +1,44 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  compress: true,
+  
+  allowedDevOrigins: ['172.16.4.194'],
+
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://127.0.0.1:8000/api/:path*",
+      },
+      {
+        source: "/uploads/:path*",
+        destination: "http://127.0.0.1:8000/uploads/:path*",
+      },
+    ];
+  },
+
+  experimental: {
+    optimizePackageImports: [
+      "framer-motion",
+      "lucide-react",
+      "@dnd-kit/core",
+      "@dnd-kit/sortable",
+      "@dnd-kit/utilities",
+    ],
+  },
+
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      { protocol: "https", hostname: "static.hiromu.top" },
+      { protocol: "https", hostname: "hiromu520.oss-cn-beijing.aliyuncs.com" },
+      { protocol: "https", hostname: "picsum.photos" },
+      { protocol: "https", hostname: "avatars.githubusercontent.com" },
+      { protocol: "http", hostname: "wfqqreader-1252317822.image.myqcloud.com" },
+    ],
+  },
+};
+
+export default nextConfig;
